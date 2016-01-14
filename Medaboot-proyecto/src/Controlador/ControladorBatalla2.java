21e1a9a3947786f83bda0b0282d1580f8087cebc
kimbo <kimbo@kimbo-PC>
2016-01-14 03:59:07 -0300

    PRUEBA

--- Medaboot-proyecto/src/Controlador/ControladorBatalla2.java
+++ Medaboot-proyecto/src/Controlador/ControladorBatalla2.java
@@ -7,6 +7,10 @@ import Vista.VistaBatalla;
 import java.awt.ComponentOrientation;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.ArrayList;
 import javax.swing.JToggleButton;
 import java.lang.System;
@@ -16,7 +20,7 @@ import java.lang.reflect.Array.*;
 // la idea es en ataque guardar el ataque de una medaparte a otr , y luego y juntando estos en lista ataques
 
 
-public class ControladorBatalla2 implements ActionListener {
+public class ControladorBatalla2 implements ActionListener, FocusListener,ItemListener {
     private VistaBatalla vb;
     private Batalla batalla;
     private ArrayList ataques;// lista que guardara todos los ataques de un turno
@@ -28,9 +32,18 @@ public class ControladorBatalla2 implements ActionListener {
         
         this.batalla=b;
         this.vb=vb;
-        
+        this.acciones1[0]=this.vb.getJtBrazoIZ1();
+        this.acciones1[1]=this.vb.getJtBrazoDE1();
+        this.acciones1[2]=this.vb.getJtPiernaIZ1();
+        this.acciones1[3]=this.vb.getJtPiernaDE1();
+        this.acciones1[4]=this.vb.getJtCabeza1();
+        this.acciones2[0]=this.vb.getJtBrazoIZ2();
+        this.acciones2[1]=this.vb.getJtBrazoDE2();
+        this.acciones2[2]=this.vb.getJtPiernaIZ2();
+        this.acciones2[3]=this.vb.getJtPiernaDE2();
+        this.acciones2[4]=this.vb.getJtCabeza2();
         this.vb.getBtConfirmarA().addActionListener(this);
-        this.vb.getBtListo().addActionListener(this);
+        this.vb.getBtListo().isSelected();
         this.vb.getJtBrazoIZ1().addActionListener(this);
        /* this.vb.getJpVida1().setValue(this.batalla.getJugador1().getSalud());
         this.vb.getJpVida2().setValue(this.batalla.getJugador2().getSalud());
@@ -92,10 +105,12 @@ public class ControladorBatalla2 implements ActionListener {
     public void realizarBatalla(){
         while(this.batalla.getJugador1().getSalud()>0 && this.batalla.getJugador2().getSalud()>0){
             for(Medaboot jugador:this.batalla.getPersonajes()){
-                while(jugador.getPh()>0){
-                    
-                
+               if(jugador.equals(this.batalla.getJugador1())){
+                   botonDisabled(acciones2, null);
                 }
+               else{
+                   botonDisabled(acciones2, null);
+               }
             }
         }
         
@@ -107,24 +122,28 @@ public class ControladorBatalla2 implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent ae) {
         
-        int parte =1;
+       /* int parte =1;
        botonDisabled (this.acciones1, (JToggleButton) ae.getSource());// se selecciona un boton, se deshabilitan los demas
        for(JToggleButton elementos : this.acciones1){// se busca el numero del boton sleeionado en el array
+           
        if (ae.getSource().equals(elementos)){
             break;
        }
        else{
            parte+=1;
        }
+       }*/
+       if(ae.getSource().equals(this.vb.getJtBrazoIZ1))){
+           if((this.vb.getJtBrazoIZ1().isSelected())){ 
+                System.out.println("kimbo");
+           }
        }
-       if (ae.getSource().equals(this.vb.getJtBrazoIZ1())){
-           System.out.println("kimbo");
-    }
+       
            
            
       
         
-      switch(parte){ // teniendo el numero alamacenado en parte , se tomaran la medaparte a la que corresponde tal numero 
+      /*switch(parte){ // teniendo el numero alamacenado en parte , se tomaran la medaparte a la que corresponde tal numero 
                      // y se guardara en una lista , definida en atributo 
             case 1: this.ataque.add(this.batalla.getJugador1().getBrazoDer());
                   break;
@@ -137,7 +156,7 @@ public class ControladorBatalla2 implements ActionListener {
             case 5: this.ataque.add(this.batalla.getJugador1().getBrazoDer());
                   break;   
             default: System.out.println("error");
-         }
+         }*/
     
     
     }
@@ -160,4 +179,26 @@ public class ControladorBatalla2 implements ActionListener {
             }
     
     }
+
+    @Override
+    public void focusGained(FocusEvent fe) {
+       //if(fe.getSource().equals(this.vb.getJtBrazoIZ1())){
+           //System.out.println("kimbo");
+       
+    }
+
+    @Override
+    public void focusLost(FocusEvent fe) {
+        this.vb.addFocusListener(this);
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent ie) {
+        if((this.vb.getJtBrazoIZ1().isSelected())){ 
+            System.out.println("kimbo");
+        
+           
+    }
+ }
 }
+        
