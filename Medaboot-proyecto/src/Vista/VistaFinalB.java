6ea01caa8cb0c145420254b9c02e8f88be4d4927
kimbo <kimbo@kimbo-PC>
2016-01-20 10:44:36 -0300

    Parte de transcurso torneo

--- Medaboot-proyecto/src/Vista/VistaFinalB.java
+++ Medaboot-proyecto/src/Vista/VistaFinalB.java
@@ -5,6 +5,7 @@
  */
 package Vista;
 
+import javax.swing.JButton;
 import javax.swing.JTextField;
 
 /**
@@ -19,6 +20,12 @@ public class VistaFinalB extends javax.swing.JPanel {
     public VistaFinalB() {
         initComponents();
     }
+
+    public JButton getBtContinuar2() {
+        return btContinuar2;
+    }
+    
+    
 
     public JTextField getTxPerdedor() {
         return txPerdedor;
@@ -51,10 +58,13 @@ public class VistaFinalB extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         txPerdedor = new javax.swing.JTextField();
+        btContinuar2 = new javax.swing.JButton();
 
         jLabel3.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
         jLabel3.setText("El Perdedor es");
 
+        txGanador.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
+        txGanador.setDisabledTextColor(new java.awt.Color(0, 0, 204));
         txGanador.setEnabled(false);
 
         jLabel4.setFont(new java.awt.Font("Times New Roman", 1, 14)); // NOI18N
@@ -68,22 +78,31 @@ public class VistaFinalB extends javax.swing.JPanel {
         jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel2.setText("El ganador es");
 
+        txPerdedor.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
+        txPerdedor.setDisabledTextColor(new java.awt.Color(0, 0, 204));
         txPerdedor.setEnabled(false);
+
+        btContinuar2.setText("Continuar");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(441, Short.MAX_VALUE)
-                .addComponent(txRecompensa, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(180, 180, 180))
             .addGroup(layout.createSequentialGroup()
                 .addGap(124, 124, 124)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(txGanador, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txPerdedor, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(441, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(txRecompensa, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(180, 180, 180))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btContinuar2)
+                        .addGap(48, 48, 48))))
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                     .addGap(422, 422, 422)
@@ -106,7 +125,9 @@ public class VistaFinalB extends javax.swing.JPanel {
                 .addComponent(txPerdedor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(78, 78, 78)
                 .addComponent(txRecompensa, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(175, 175, 175))
+                .addGap(90, 90, 90)
+                .addComponent(btContinuar2)
+                .addGap(62, 62, 62))
             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(layout.createSequentialGroup()
                     .addContainerGap()
@@ -123,6 +144,7 @@ public class VistaFinalB extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btContinuar2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
